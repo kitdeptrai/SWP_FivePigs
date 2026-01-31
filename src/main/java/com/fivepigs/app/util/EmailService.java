7e575ba917e1d262bde9c36c21c23302181b8655
nguyenhung14012k5 <nguyenhung14012k5@gmail.com>
2026-01-31 15:38:23 +0700

    fix login and register function

--- src/main/java/com/fivepigs/app/util/EmailService.java
+++ src/main/java/com/fivepigs/app/util/EmailService.java
@@ -9,14 +9,33 @@ import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 
+import java.io.InputStream;
 import java.util.Properties;
 
 public class EmailService {
 
-    private static final String FROM_EMAIL = "fivepigs@gmail.com";
-    private static final String PASSWORD = "nhfv qedp wvro qgqt";
+    private static final Properties emailProperties = new Properties();
+
+    static {
+        try (InputStream input = EmailService.class.getClassLoader().getResourceAsStream("email.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find email.properties");
+            } else {
+                emailProperties.load(input);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load email properties", e);
+        }
+    }
+
+    private static final String FROM_EMAIL = emailProperties.getProperty("email.from");
+    private static final String PASSWORD = emailProperties.getProperty("email.password");
 
     public static void sendEmail(String toEmail, String subject, String body) {
+        sendHtmlEmail(toEmail, subject, "<p>" + body + "</p>");
+    }
+
+    public static void sendHtmlEmail(String toEmail, String subject, String htmlBody) {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
@@ -35,11 +54,12 @@ public class EmailService {
             message.setFrom(new InternetAddress(FROM_EMAIL));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
             message.setSubject(subject);
-            message.setText(body);
+            message.setContent(htmlBody, "text/html; charset=utf-8");
 
             Transport.send(message);
         } catch (MessagingException e) {
             e.printStackTrace();
+            throw new RuntimeException("Failed to send email", e);
         }
     }
 }
