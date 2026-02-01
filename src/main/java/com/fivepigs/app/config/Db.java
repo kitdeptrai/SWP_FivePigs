242df15514b303b8516f137532546ad05ebb4158
nguyenhung14012k5 <nguyenhung14012k5@gmail.com>
2026-02-01 10:29:52 +0700

    fix email service

--- src/main/java/com/fivepigs/app/config/Db.java
+++ src/main/java/com/fivepigs/app/config/Db.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public final class Db {
     // Cấu hình MySQL - THAY ĐỔI THEO CẤU HÌNH CỦA BẠN
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/fivepigs?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/fivepigs?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASS = "123456";
 
